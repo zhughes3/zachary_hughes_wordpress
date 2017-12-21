fc17144c689915ddfdc4960df0aa95750fbd0cab
zhughes3 <zhughes3@gmail.com>
2017-12-21 01:01:51 -0500

    added the wordpress loop to index.php

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Zachary Hughes Wordpress Theme</title>
     <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery-3.2.1.min.js';?>"></script>
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'?>"></script>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'?>">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js';?>"></script>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css';?>">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
 </head>
 
 <body>
