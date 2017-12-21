5a9ab8139fc92ecbf14b9486b405d11953a40273
zhughes3 <zhughes3@gmail.com>
2017-12-21 00:52:34 -0500

    wordpress theme scaffolding; base header.php with js and css links loaded

---
+++ header.php
@@ -0,0 +1,16 @@
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Zachary Hughes Wordpress Theme</title>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery-3.2.1.min.js';?>"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'?>"></script>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'?>">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+</head>
+
+<body>
+<div id="zh_header" class="jumbotron">
+    <h1>Header</h1>
+</div>
+<div class="container">
